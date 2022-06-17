542e0812d5a0ee9e7cde6945b3f9bda7c6753fd6
farfi <=>
2022-06-17 20:12:38 +0200

    added support for non-graphic mode

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <time.h>
 #include <string>
@@ -7,11 +8,16 @@
 #include <allegro5/allegro_ttf.h>
 #include <allegro5/allegro_primitives.h>
 
+// #define GRAPHIC_MODE
 
 ALLEGRO_FONT* font;
 ALLEGRO_DISPLAY* display;
 ALLEGRO_EVENT_QUEUE* queue;
 ALLEGRO_TIMER* timer;
+
+
+int rand_seed = 0xCAFFE;
+
 
 int initial_fill_perc = 51;
 
@@ -24,13 +30,14 @@ const int CELL_HEIGHT = 3;
 const int DISPLAY_WIDTH = COLS * CELL_WIDTH;
 const int DISPLAY_HEIGHT = ROWS * CELL_HEIGHT;
 
-
+int stopwatch_last_gen = 20;
+int generation = 0;
 
 const uint8_t neighbour_radius = 3;
 
 const int max_neighbours = (neighbour_radius * (neighbour_radius + 1) * 4);
 const int half_neighbours = max_neighbours / 2;
-// 
+
 const int roughness = 4;
 
 
@@ -55,50 +62,76 @@ void draw_grid();
 
 int main(int argc, char const* argv[])
 {
+	#ifdef GRAPHIC_MODE
 	initialize();
+	#endif
 	initialize_random_grid();
+
+	auto t1 = std::chrono::high_resolution_clock::now();
+
 
 	bool running = true;
 	while(running) {
+		#ifdef GRAPHIC_MODE
 		ALLEGRO_EVENT event;
 		al_wait_for_event(queue, &event);
 		if(event.type == ALLEGRO_EVENT_KEY_UP || event.type == ALLEGRO_EVENT_DISPLAY_CLOSE)
 			running = false;
-		else if(event.type == ALLEGRO_EVENT_TIMER) {
+		else if(event.type == ALLEGRO_EVENT_TIMER)
+			#endif
+		{
 			frame_update();
-		}
+			if(generation++ == stopwatch_last_gen) {
+				auto t2 = std::chrono::high_resolution_clock::now();
 
+				/* Getting number of milliseconds as an integer. */
+				auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+				std::cout << ms_int.count() << std::endl;
+				running = false;
+			}
+		}
 	}
+	#ifdef GRAPHIC_MODE
 	terminate();
+	#endif
 
 	return 0;
 }
 
 void initialize() {
 	if(!al_init()) fprintf(stderr, "Failed to initialize allegro.\n");
+	if(!al_install_keyboard()) fprintf(stderr, "Failed to install keyboard.\n");
+	#ifdef GRAPHIC_MODE
 	if(!al_init_font_addon()) fprintf(stderr, "Failed to initialize font addon.\n");
 	if(!al_init_ttf_addon()) fprintf(stderr, "Failed to initialize ttf addon.\n");
 	if(!al_init_primitives_addon()) fprintf(stderr, "Failed to initialize primitives addon.\n");
-	if(!al_install_keyboard()) fprintf(stderr, "Failed to install keyboard.\n");
 
 	font = al_load_ttf_font("fonts/Roboto/Roboto-Medium.ttf", 20, 0);
 	display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+	#endif
 	queue = al_create_event_queue();
 	timer = al_create_timer(1.0 / 60);
 
+	#ifdef GRAPHIC_MODE
 	if(!font) fprintf(stderr, "Failed load font.\n");
 	if(!display) fprintf(stderr, "Failed initialize display.\n");
+	#endif
 	if(!queue) fprintf(stderr, "Failed create queue.\n");
 	if(!timer) fprintf(stderr, "Failed to create timer.\n");
 
-	al_register_event_source(queue, al_get_keyboard_event_source());
+	#ifdef GRAPHIC_MODE
 	al_register_event_source(queue, al_get_display_event_source(display));
+	#endif
+	al_register_event_source(queue, al_get_keyboard_event_source());
 	al_register_event_source(queue, al_get_timer_event_source(timer));
 	al_start_timer(timer);
 }
 
 void initialize_random_grid() {
-	srand(time(0));
+	if(rand_seed)
+		srand(rand_seed);
+	else srand(time(0));
+
 	for(int i = 0; i < ROWS; i++) {
 		for(int j = 0; j < COLS; j++) {
 			read_grid[at(i, j)] = (rand() % 100) < initial_fill_perc;
@@ -116,14 +149,17 @@ void terminate()
 }
 
 void frame_update() {
+	#ifdef GRAPHIC_MODE
 	al_flip_display();
 	al_clear_to_color(floor_color);
+	#endif
 
 	update_grid();
+	flip_grid();
 
+	#ifdef GRAPHIC_MODE
 	draw_grid();
-
-
+	#endif
 }
 
 int get_neighbour_walls(int y, int x) {
@@ -160,7 +196,6 @@ void flip_grid() {
 
 
 void draw_grid() {
-	flip_grid();
 	for(int i = 0; i < ROWS; i++) {
 		for(int j = 0; j < COLS; j++) {
 			if(read_grid[at(i, j)]) {
