7409002a1618821c8f889a5333cd6063e7e39211
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-07-08 01:18:11 +0200

    avgtime.pl improvements

--- scripts/avgtime.pl
+++ scripts/avgtime.pl
@@ -1,8 +1,9 @@
+#! /usr/bin/perl
 my $executions = 0;
 my $tot_time = 0;
 
-my $results_file = "./results.txt";
-
+# my $results_file = "./results.txt";
+my $results_file = shift @ARGV or die "$! provide a file to read from";
 open(FILE2, "<", $results_file) or die $!;
 
 for (<FILE2>){
@@ -15,4 +16,4 @@ close $FILE2;
 
 print "$executions executions in $tot_time ms\n";
 print $tot_time/$executions;
-print "ms avrage\n";
+print " ms avrage\n";
