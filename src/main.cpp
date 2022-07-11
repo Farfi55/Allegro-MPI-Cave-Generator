d800bce0cba4863a565f13bdc9252948d8f463ff
Alessio "Farfi" Farfaglia <alessio.farfaglia@gmail.com>
2022-07-11 17:41:16 +0200

    more timings data

--- src/main.cpp
+++ src/main.cpp
@@ -19,6 +19,7 @@
 #define ROOT_RANK 0
 
 Config* cfg;
+std::string config_file_path = "./config/default.cfg";
 
 uint8_t* write_grid;
 uint8_t* read_grid;
@@ -58,6 +59,13 @@ int outer_grid_size;
 
 int generation = 0;
 bool is_running = true;
+
+double total_time = 0;
+double communication_time = 0;
+double generation_time = 0;
+double draw_time = 0;
+double start_time, end_time;
+double* frame_times;
 
 #ifdef PARALLEL_MODE
 int neighbours_ranks[3][3];
@@ -113,11 +121,17 @@ void update_grid();
 void flip_grid();
 void check_generic_settings();
 void no_graphic_loop();
+void write_header(std::ofstream& file);
+void write_result(std::ofstream& file);
+
+
+
 
 // graphic only
 void graphic_initialize();
 void serial_draw_grid();
 void check_graphic_settings();
+void graphic_serial_loop();
 void graphic_parallel_loop();
 
 // parallel only
@@ -148,36 +162,33 @@ void receive_corners();
 int main(int argc, char const* argv[])
 {
 
-	{
-		std::string configPath;
-		int x_threads, y_threads;
-		for(int i = 0; i < argc; i++) {
-			if(argv[i] == std::string("-c") && i + 1 < argc) {
-				configPath = argv[++i];
-			}
-			else if(argv[i] == std::string("-x") && i + 1 < argc) {
-				x_threads = std::stoi(argv[++i]);
-				std::cout << "x_threads: " << x_threads << std::endl;
-			}
-			else if(argv[i] == std::string("-y") && i + 1 < argc) {
-				y_threads = std::stoi(argv[++i]);
-				std::cout << "y_threads: " << y_threads << std::endl;
-			}
-			// else if(argv[i] == std::string("-g")) {
-			// 	cfg->graphic_mode = true;
-			// }
-			// else if(argv[i] == std::string("-p")) {
-			// 	cfg->parallel_mode = true;
-			// }
-
+	int x_threads = 0, y_threads = 0;
+	for(int i = 0; i < argc; i++) {
+		if(argv[i] == std::string("-c") && i + 1 < argc) {
+			config_file_path = argv[++i];
 		}
-
-		initialize(configPath, x_threads, y_threads);
+		else if(argv[i] == std::string("-x") && i + 1 < argc) {
+			x_threads = std::stoi(argv[++i]);
+			std::cout << "x_threads: " << x_threads << std::endl;
+		}
+		else if(argv[i] == std::string("-y") && i + 1 < argc) {
+			y_threads = std::stoi(argv[++i]);
+			std::cout << "y_threads: " << y_threads << std::endl;
+		}
+		// else if(argv[i] == std::string("-g")) {
+		// 	cfg->graphic_mode = true;
+		// }
+		// else if(argv[i] == std::string("-p")) {
+		// 	cfg->parallel_mode = true;
+		// }
 	}
 
 
-	auto start_time = std::chrono::high_resolution_clock::now();
+	initialize(config_file_path, x_threads, y_threads);
 
+
+	// auto start_time = std::chrono::high_resolution_clock::now();
+	start_time = MPI_Wtime();
 
 	while(is_running) {
 		#ifdef GRAPHIC_MODE
@@ -190,22 +201,44 @@ int main(int argc, char const* argv[])
 		no_graphic_loop();
 		#endif // GRAPHIC_MODE
 	}
-	auto end_time = std::chrono::high_resolution_clock::now();
-	auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 
-	std::cout << ms_int.count() << std::endl;
-	if(!(cfg->results_file_path.empty())) {
-		std::ofstream results_file;
-		results_file.open(cfg->results_file_path, std::ios::ate);
-		results_file << ms_int.count() << std::endl;
-		results_file.close();
+	end_time = MPI_Wtime();
+	total_time = end_time - start_time;
+	// auto end_time = std::chrono::high_resolution_clock::now();
+	// auto ms_int = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+
+	if(my_rank == ROOT_RANK) {
+		std::cout << "Total time: " << total_time << std::endl;
+		std::cout << "Communication time: " << communication_time << std::endl;
+		std::cout << "Generation time: " << generation_time << std::endl;
+		std::cout << "Draw time: " << draw_time << std::endl;
+
+		if(cfg->results_file_path.empty()) {
+			std::cout << "No results file path specified" << std::endl;
+		}
+		else {
+			std::ofstream file;
+			if(std::filesystem::exists(cfg->results_file_path)) {
+				file.open(cfg->results_file_path, std::ios::app);
+			}
+			else {
+				std::cout << "Creating results file" << std::endl;
+				file.open(cfg->results_file_path);
+				write_header(file);
+			}
+			write_result(file);
+			file.close();
+
+			std::cout << "Results written to " << cfg->results_file_path << std::endl;
+		}
 	}
+
+
 
 	terminate();
 
 	return 0;
 }
-
 
 /*
  * ==================================================================================
@@ -214,6 +247,7 @@ int main(int argc, char const* argv[])
  *  --------------------------------------------------------------------------------
  * ==================================================================================
  */
+
 void initialize(std::string configPath, int x_threads, int y_threads) {
 	if(!configPath.empty())
 		cfg = new Config(configPath);
@@ -229,6 +263,7 @@ void initialize(std::string configPath, int x_threads, int y_threads) {
 	if(x_threads) cfg->x_threads = x_threads;
 	if(y_threads) cfg->y_threads = y_threads;
 
+	frame_times = new double[cfg->last_generation];
 
 	// checkGeneralSettings();
 
@@ -411,6 +446,7 @@ void terminate()
 	delete cfg;
 	delete[] read_grid;
 	delete[] write_grid;
+	delete[] frame_times;
 
 	#ifdef PARALLEL_MODE
 	if(my_rank == ROOT_RANK) {
@@ -553,23 +589,33 @@ void no_graphic_loop() {
 
 
 void frame_update() {
+	double frame_start_time = MPI_Wtime();
 	#ifdef GRAPHIC_MODE
 	if(my_rank == ROOT_RANK) {
+		double start_draw_time = MPI_Wtime();
 		al_flip_display();
 		al_clear_to_color(wall_color);
 
 		#ifdef PARALLEL_MODE 
 		//receive the grid from the other processes
+		double receive_start_time = MPI_Wtime();
 		gather_grid();
+		communication_time += MPI_Wtime() - receive_start_time;
+
 		parallel_draw_grid();
 		#else  
 		serial_draw_grid();
 		#endif // PARALLEL_MODE
+
+		double end_draw_time = MPI_Wtime();
+		draw_time += end_draw_time - start_draw_time;
 	}
 	else {
 		// send read_grid to root
 		#ifdef PARALLEL_MODE
+		double receive_start_time = MPI_Wtime();
 		gather_grid();
+		communication_time += MPI_Wtime() - receive_start_time;
 		#endif // PARALLEL_MODE
 
 	}
@@ -577,6 +623,7 @@ void frame_update() {
 
 	#ifdef PARALLEL_MODE
 	// send columns to other processes
+	double comms_start_time = MPI_Wtime();
 	send_columns();
 
 	// send rows to other processes
@@ -593,15 +640,21 @@ void frame_update() {
 
 	// receive corners from other processes
 	receive_corners();
-
+	communication_time += MPI_Wtime() - comms_start_time;
 
 	#endif // PARALLEL_MODE
+
+	double generation_start_time = MPI_Wtime();
 	update_grid();
+	generation_time += MPI_Wtime() - generation_start_time;
 	std::swap(read_grid, write_grid);
 
-	#ifdef PARALLEL_MODE
-	MPI_Barrier(cave_comm);
-	#endif // PARALLEL_MODE	
+	// #ifdef PARALLEL_MODE
+	// MPI_Barrier(cave_comm);
+	// #endif // PARALLEL_MODE	
+
+	double frame_end_time = MPI_Wtime();
+	frame_times[generation] = frame_end_time - frame_start_time;
 }
 
 int get_neighbour_walls(int y, int x) {
@@ -653,8 +706,6 @@ void parallel_draw_grid() {
 		#ifdef DEBUG_MODE
 		int proc_x2 = ((proc % cfg->x_threads) + 1) * my_inner_cols + edge_offset;
 		int proc_y2 = ((proc / cfg->x_threads) + 1) * my_inner_rows + edge_offset;
-
-		std::cout << "proc: " << proc << " at " << proc_x << " " << proc_y << std::endl;
 		#endif // DEBUG_MODE
 
 		for(int i = 0; i < my_inner_rows; i++) {
@@ -879,3 +930,46 @@ void receive_corners() {
 
 #endif // PARALLEL_MODE
 
+
+
+
+void write_header(std::ofstream& file) {
+	std::string separator = ",";
+	file << "total_time" << separator
+		<< "communication_time" << separator
+		<< "generation_time" << separator
+		<< "draw_time" << separator
+		<< "start_time" << separator
+		<< "end_time" << separator
+		<< "n_procs" << separator
+		<< "x_threads" << separator
+		<< "y_threads" << separator
+		<< "config_file_path" << separator
+		<< "frame_timings" << std::endl;
+}
+
+
+void print_frame_times(std::ostream& os) {
+	os << '[';
+	for(int i = 0; i < cfg->last_generation; i++) {
+		os << frame_times[i] << ' ';
+	}
+	os << ']';
+}
+
+void write_result(std::ofstream& file) {
+	std::string separator = ",";
+	file << total_time << separator
+		<< communication_time << separator
+		<< generation_time << separator
+		<< draw_time << separator
+		<< start_time << separator
+		<< end_time << separator
+		<< n_procs << separator
+		<< cfg->x_threads << separator
+		<< cfg->y_threads << separator
+		<< config_file_path << separator;
+	print_frame_times(file);
+
+	file << std::endl;
+}
